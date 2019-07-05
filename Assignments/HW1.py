c5e676fdab948b3297687711a5b024f0dc08bac8
Ikey Benzaken <ikey.benz@gmail.com>
2019-07-05 01:05:47 -0400

    Added HW1

--- Assignments/HW1.py
+++ Assignments/HW1.py
@@ -22,21 +22,45 @@ except:
 # 3- **Add more complexity to your models in (1) and (2)** and compare with previous results
 
 # %%
-# Build model for linear regression:
 import numpy as np
 import pandas as pd
+
 from keras.models import Sequential
 from keras.layers import Dense
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
+
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.datasets import load_boston
 
+# %% Loading Data
+data = load_boston()
+X = pd.DataFrame(data=data.data, columns=data.feature_names)
+y = pd.DataFrame(data=data.target, columns=['MEDV'])
 
-# %%
-df = load_boston()
-X = pd.DataFrame(df.data)
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# %% Building Model
+regressor = Sequential([
+    Dense(activation='linear', input_dim=13,
+          units=1, kernel_initializer='uniform'),
+    Dense(activation='linear', units=1, kernel_initializer='uniform')
+])
+regressor.compile(optimizer='adam', loss='mse', metrics=['mse'])
+
+# %% Fitting Model
+regressor.fit(X_train, y_train, batch_size=1, epochs=100)
 
 
-# %%
+# %% Making Predictions
+y_pred = regressor.predict(X_test)
+
+# %% Comparing to SKLearn.Linreg
+linreg = LinearRegression().fit(X_train, y_train)
+
+print(linreg.score(X_test, y_test))
+print(r2_score(y_test, y_pred))
